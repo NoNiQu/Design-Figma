4db3820d5740b75106453198ce79d49a271b2155
NoNiQu <marcos.workstation@gmail.com>
2024-12-02 13:55:35 +0100

    finish

---
+++ README.md
@@ -0,0 +1,26 @@
+# P 3.1 Maquetación con Vite y SASS
+> Nombre: Marcos Alarcón Alguacil
+
+> Curso: S2DAW
+
+> Modulo: DIW
+
+## 1. Descripción
+
+Esta práctica consiste en la creación de una página web para un juego de navegador. El diseño inicial se realizó en una práctica previa utilizando Figma. Sin embargo, como parte de este proyecto, se rediseñó el diseño original y se ajustó la guía de estilo para llegar a un diseño final más adecuado.
+
+Originalmente, la idea incluía un diseño adaptable que permitiera la rotación automática en dispositivos móviles. Debido a la complejidad técnica y el tiempo disponible, se decidió simplificar el enfoque y rediseñar todo para adaptarse mejor a las limitaciones del proyecto.
+
+### 1.1 Instrucciones de instalar
+
+1. Clonar el respositorio con -> git clone 'link del repositorio'.
+2. En el repositorio abrimos el terminar y lanzamos el siguiente comando -> npm install (para instalar las dependencia de node). (Si no tienes node te lo descargas).
+3. Despues instalamos sass -> npm install -g sass.
+4. Para abrir nuestro index utilizaremos -> npm run dev y hacemos click en link que ponga localhost.
+
+
+## 2. Enlace de interés
+
+[Enlace a Github](https://noniqu.github.io/Design-Figma/)
+
+[Enlace a Figma](https://www.figma.com/design/TYP0NbM4YEKoyFJrETvIHt/Style-Guide?node-id=1-2&node-type=frame)
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,48 @@
-import "../style/main.scss"
+import "../style/main.scss";
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  const joinBetaButton = document.getElementById("joinBetaButton");
+  const playDemo = document.getElementById("play");
+  const registrarseForm = document.getElementById("registrarse");
+
+  function toggleForm() {
+    if (registrarseForm.classList.contains("hidden")) {
+      registrarseForm.classList.remove("hidden");
+      playDemo.classList.add("hidden");
+    } else {
+      registrarseForm.classList.add("hidden");
+      playDemo.classList.remove("hidden");
+    }
+  }
+
+  joinBetaButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    toggleForm();
+  });
+
+  function closeFormOnClickOutside(event) {
+    if (
+      !registrarseForm.contains(event.target) &&
+      !joinBetaButton.contains(event.target)
+    ) {
+      registrarseForm.classList.add("hidden");
+      playDemo.classList.remove("hidden");
+    }
+  }
+
+  document.addEventListener("click", closeFormOnClickOutside);
+
+  const volumeIcon = document.getElementById("volumeIcon");
+
+  volumeIcon.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    if (volumeIcon.querySelector("i").classList.contains("fa-volume-up")) {
+      volumeIcon.querySelector("i").classList.remove("fa-volume-up");
+      volumeIcon.querySelector("i").classList.add("fa-volume-off");
+    } else {
+      volumeIcon.querySelector("i").classList.remove("fa-volume-off");
+      volumeIcon.querySelector("i").classList.add("fa-volume-up");
+    }
+  });
+});
